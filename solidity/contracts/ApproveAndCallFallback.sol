bb6996348f17d67ce71f703f0d881c61ae021f67
Cryptogenik <larceniii@gmail.com>
2020-09-16 21:17:09 -0500

    Small update

--- solidity/contracts/ApproveAndCallFallback.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity ^0.6.0;
-
-
-interface ApproveAndCallFallBack {
-    function receiveApproval(address from, uint256 tokens, address token, bytes calldata data) external;
-}
--- solidity/migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const DeployAll = artifacts.require("DeployAll");
-
-module.exports = function(deployer) {
-  deployer.deploy(DeployAll);
-};
